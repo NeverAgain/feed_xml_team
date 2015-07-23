9611addabbcfac1c23980416bafa313cac23e45f
Edward Zhang <edwardforcpu@gmail.com>
2015-07-23 15:36:59 -0400

    updated the base url to xmlteam's production url; bumped the version

--- lib/feed_xml_team/base.rb
+++ lib/feed_xml_team/base.rb
@@ -1,3 +1,3 @@
 module FeedXmlTeam
-  API_BASE_URL = 'http://feed5.xmlteam.com/api'
+  API_BASE_URL = 'http://sw5feed.xmlteam.com/api'
 end
--- lib/feed_xml_team/version.rb
+++ lib/feed_xml_team/version.rb
@@ -1,3 +1,3 @@
 module FeedXmlTeam
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 end
